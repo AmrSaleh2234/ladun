9a5f49fd8b71483940382f0eda4002730d5507a7
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-02-05 17:41:44 +0200

    land page

---
+++ resources/views/landpage.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>land page </title>
+    @vite('resources/css/app.css')
+    <style>
+        #banner {
+            background-image: url("img/background.png");
+            height: 100vh;
+            background-position: center; /* Center the image */
+            background-repeat: no-repeat; /* Do not repeat the image */
+            background-size: cover; /* Resize the background image to cover the entire container */
+        }
+    </style>
+</head>
+<body>
+{{--<video autoplay muted loop id="myVideo">--}}
+{{--    <source src="rain.mp4" type="video/mp4">--}}
+{{--</video>--}}
+<header id="banner">
+    <nav class="px-24 py-10">
+        <div class="flex justify-between items-center">
+
+
+            <ul class="flex justify-between w-80 text-white">
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+
+            </ul>
+            <img class="w-20" src="img/logo.png">
+        </div>
+    </nav>
+</header>
+</body>
+</html>
